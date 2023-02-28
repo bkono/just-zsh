030c3a508337805213b224eef1bc142c4507c9f9
Bryan Konowitz <bryan@kono.sh>
2023-02-28 11:21:04 -0800

    refactor: cleaned up install.sh and prepped for more dotfiles support

--- install.sh
+++ install.sh
@@ -10,6 +10,41 @@ function brew_install() {
   fi
 }
 
+# symlink a file or directory to home. first argument is required and references the file or directory source, relative to
+# the current dir. second argument is optional. if provided it will be the target for linking. if not provided, default
+# target is `$HOME/.<first argument>`. When the first argument is a directory and the target already exists, the
+# existing directory will be backed up. When the second argument contains a path, the full target
+# directory will be created if it does not exist.
+function link() {
+  if [[ -z $1 ]]; then
+    echo "link requires at least one argument"
+    return 1
+  fi
+
+
+  local source=$PWD/$1
+  local target
+
+  if [[ -z $2 ]]; then
+    target=$HOME/.$1
+  else
+    if [[ $(basename $2) != $2 ]]; then
+      local target_dir=$(dirname $2)
+      [[ ! -d $HOME/$target_dir ]] && mkdir -p $HOME/$target_dir
+    fi
+    target=$HOME/$2
+  fi
+
+  echo -n "Linking $1 to $target..."
+
+  if [[ -d $source ]]; then
+    [[ -d $target && ! ($target -ef $source) ]] && echo -n " $target exists to $target-bak..." && mv $target $target-bak
+  fi
+
+  ln -snfh $source $target
+  echo " done."
+}
+
 if [[ ! -d $HOME/.zgenom ]]; then
   echo "Installing zgenom..."
   git clone https://github.com/jandamm/zgenom.git "${HOME}/.zgenom"
@@ -22,15 +57,9 @@ for prereq in $prereqs; do
 done
 
 # link the things
-echo "Linking .zshrc..."
-ln -snfh $PWD/zshrc $HOME/.zshrc
+link zshrc
+link zsh
+link starship.toml .config/starship.toml
 
-echo "Linking .zsh folder..."
-[[ -d $HOME/.zsh && ! ($HOME/.zsh -ef $PWD/zsh) ]] && echo "Moving old .zsh folder to backup..." && mv $HOME/.zsh $HOME/zsh-bak
-ln -snfh $PWD/zsh $HOME/.zsh
-
-echo "Linking starship.toml..."
-[[ ! -d $HOME/.config ]] && mkdir $HOME/.config
-ln -snfh $PWD/starship.toml $HOME/.config/starship.toml
-
-echo "... done. Reload your terminal or run 'source ~/.zshrc'"
+echo ""
+echo "... all set. Reload your terminal or open a new tab."
